014a88c5a4badcea0b373924390532f949fa3e92
Peter Perceval <pmperceval@gmail.com>
2017-08-09 21:50:22 -0500

    New Website for github pages

---
+++ js/overlay.js
@@ -0,0 +1,95 @@
+(function() {
+    var triggerBttn = document.getElementById( 'trigger-overlay' ),
+        triggerBttnTwo = document.getElementById( 'trigger-overlay-two' ),
+        triggerBttnThree = document.getElementById( 'trigger-overlay-three' ),
+        triggerBttnFour = document.getElementById( 'trigger-overlay-four' ),
+        triggerBttnFive = document.getElementById( 'trigger-overlay-five' ),
+        triggerBttnSix = document.getElementById( 'trigger-overlay-six' ),
+        triggerBttnSeven = document.getElementById( 'trigger-overlay-seven' ),
+        triggerBttnEight = document.getElementById( 'trigger-overlay-eight' ),
+        triggerBttnNine = document.getElementById( 'trigger-overlay-nine' ),
+        triggerBttnTen = document.getElementById( 'trigger-overlay-ten' ),
+        triggerBttnEleven = document.getElementById( 'trigger-overlay-eleven' ),
+        triggerBttnTwelve = document.getElementById( 'trigger-overlay-twelve' ),
+        triggerBttnThirteen = document.getElementById( 'trigger-overlay-thirteen' ),
+        triggerBttnFourteen = document.getElementById( 'trigger-overlay-fourteen' ),
+
+        overlay = document.querySelector( 'div.overlay' ),
+        closeBttn = overlay.querySelector( 'a.overlay-close' );
+    transEndEventNames = {
+        'WebkitTransition': 'webkitTransitionEnd',
+        'MozTransition': 'transitionend',
+        'OTransition': 'oTransitionEnd',
+        'msTransition': 'MSTransitionEnd',
+        'transition': 'transitionend'
+    },
+        transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+        support = { transitions : Modernizr.csstransitions };
+
+    function toggleOverlay() {
+        if( classie.has( overlay, 'open' ) ) {
+            classie.remove( overlay, 'open' );
+            classie.add( overlay, 'close' );
+            var onEndTransitionFn = function( ev ) {
+                if( support.transitions ) {
+                    if( ev.propertyName !== 'visibility' ) return;
+                    this.removeEventListener( transEndEventName, onEndTransitionFn );
+                }
+                classie.remove( overlay, 'close' );
+            };
+
+
+
+            if( support.transitions ) {
+                overlay.addEventListener( transEndEventName, onEndTransitionFn );
+            }
+            else {
+                onEndTransitionFn();
+            }
+
+            $( "body" ).removeClass( "noscroll" );
+
+        }
+        else if( !classie.has( overlay, 'close' ) ) {
+            classie.add( overlay, 'open' );
+            $( "body" ).addClass( "noscroll" );
+        }
+
+
+
+        $("div.overlay").animate({ scrollTop: 0 }, "slow");
+
+
+    }
+
+
+    //esc key to close overlay
+    $(document).bind('keydown',function(e){
+        if ( e.which == 27 ) {
+            toggleOverlay();
+        };
+    });
+
+
+
+    closeBttn.addEventListener( 'click', toggleOverlay );
+
+    triggerBttn.addEventListener( 'click', toggleOverlay );
+    triggerBttnTwo.addEventListener( 'click', toggleOverlay );
+    triggerBttnThree.addEventListener( 'click', toggleOverlay );
+    triggerBttnFour.addEventListener( 'click', toggleOverlay );
+    triggerBttnFive.addEventListener( 'click', toggleOverlay );
+    triggerBttnSix.addEventListener( 'click', toggleOverlay );
+    triggerBttnSeven.addEventListener( 'click', toggleOverlay );
+    triggerBttnEight.addEventListener( 'click', toggleOverlay );
+    triggerBttnNineddEventListener( 'click', toggleOverlay );
+    triggerBttnTen.addEventListener( 'click', toggleOverlay );
+    triggerBttnEleven.addEventListener( 'click', toggleOverlay );
+    triggerBttnTwelve.addEventListener( 'click', toggleOverlay );
+    triggerBttnThirteen.addEventListener( 'click', toggleOverlay );
+    triggerBttnFourteen.addEventListener( 'click', toggleOverlay );
+    
+
+
+    return false;
+})();
